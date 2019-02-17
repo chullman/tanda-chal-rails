912c7e5891e04b371410d9b62571478dcb94fb6c
Chris Hullman <christopherhullman@gmail.com>
2019-02-17 21:12:50 +1000

    Devise initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :shifts
-  resources :organisations
+  devise_for :users
+  #resources :shifts
+  #resources :organisations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/home', to: 'home#index', as: 'home'
+  root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,12 +34,17 @@ ActiveRecord::Schema.define(version: 2019_02_17_052128) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email_address"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.bigint "organisation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "shifts", "users"
