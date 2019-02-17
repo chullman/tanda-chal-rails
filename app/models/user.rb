01e10d609bb9324da5ce1200f3f7f72f33230839
Chris Hullman <christopherhullman@gmail.com>
2019-02-17 20:36:26 +1000

    Destroyed user scaffold to make way for Devise auth

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  belongs_to :Organisation, optional: true
-  has_many :shifts
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :shifts
-  resources :users
   resources :organisations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
