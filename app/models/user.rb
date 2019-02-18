5741fa906b9b52c97de652ae4042c9fc9455f3d8
Chris Hullman <christopherhullman@gmail.com>
2019-02-18 14:12:44 +1000

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   belongs_to :organisation, optional: true
   has_many :shifts
+
+  validate :validate_organisation_id, if: :organisation_id? # Because organisation_id can be nil, we are skipping validation on nil records
+
+  private
+
+  # Ensures that the organisation_id in the User table actually exists in the Organisation table
+  def validate_organisation_id
+    errors.add(:organisation_id, "is invalid") unless Organisation.exists?(self.organisation_id)
+  end
+
 end
