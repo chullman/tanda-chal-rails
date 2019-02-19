f30a434d25e09d53f472b5cff2f844076fc8055a
Chris Hullman <christopherhullman@gmail.com>
2019-02-19 13:12:21 +1000

    added validation to shifts model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,12 @@
 class Shift < ApplicationRecord
   belongs_to :User
+
+  validate :validate_user_id
+
+  private
+
+  # Ensures that the user_id in the Shifts table actually exists in the User table
+  def validate_user_id
+    errors.add(:user_id, "is invalid") unless User.exists?(self.user_id)
+  end
 end
