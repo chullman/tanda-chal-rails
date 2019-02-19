1d4d55b1e19efb8dae0d3e3eff6ba5e6c6ce61c8
Chris Hullman <christopherhullman@gmail.com>
2019-02-19 14:39:48 +1000

    initial commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,22 @@ class HomeController < ApplicationController
   def index
     if (user_signed_in?)
       @userOrganisation = Organisation.where(id: current_user.organisation_id).first
+
+      if (!(@userOrganisation.nil?))
+        @allUsersAtOrganisation = User.where(organisation_id: @userOrganisation.id).all
+
+        @shiftsForAllUsersAtOrg = Hash.new
+
+        @allUsersAtOrganisation.each do |userAtOrganisation|
+          @shiftsForAllUsersAtOrg[userAtOrganisation.id] = Shift.where(user_id: userAtOrganisation.id).all
+        end
+
+      else
+        @allUsersAtOrganisation = nil  
+      end
     else
       @userOrganisation = nil
+      @allUsersAtOrganisation = nil
     end
 
   end
