223291dbc67115bee55bdbcc966b3c7f27e43d23
Chris Hullman <christopherhullman@gmail.com>
2019-02-20 14:14:23 +1000

    hopefully fixed default from mail error in prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default(from: "tanda-chal-rails@heroku.com")
+  config.action_mailer.default_options = {from: "tanda-chal-rails@heroku.com"}
   config.action_mailer.default_url_options = { :host => 'https://tanda-chal-rails.herokuapp.com' } 
   config.action_mailer.perform_deliveries = true 
   ActionMailer::Base.smtp_settings = {
